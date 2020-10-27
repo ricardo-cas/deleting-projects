47e89e11c3ee9787a11fdefda7fc60754befd3b6
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-27 00:12:07 -0300

    changes were made

--- list_repositories.py
+++ list_repositories.py
@@ -12,14 +12,26 @@ user = g.get_user()
 login = user.login
 
 
+
 # Methods
 
 def main():
     show_user()
+    show_repos()
+    show_public_repos()
 
 # show user logged
 def show_user():
     print(f'\n--------------------Login: {login} --------------------')
+
+def show_repos():
+    for repo in user.get_repos():
+        print(f'\n {repo}')
+
+def show_public_repos():
+    print(f'Total de repositórios publicos: {user.public_repos()}')
+ 
+
 
 # Main Python command method
 if __name__ == "__main__":
